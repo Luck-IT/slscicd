de737c5878afa2263445c62aba1934f84fbf7d99
wenjun wu <wenjun.wu@lotusflare.com>
2024-04-16 15:21:09 +0800

    service-a:change index.js

--- service-a/index.js
+++ service-a/index.js
@@ -5,6 +5,7 @@ module.exports.handler = async (event) => {
 
   console.log('in SERVICE-A....');
   console.log(`SHARED_VALUE:${process.env.SHARED_VALUE}`);
+  console.log(`STAGE: ${process.env.stage}`)
   return {
     statusCode: 200,
     body: JSON.stringify(
